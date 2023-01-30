4926a7e9a64d47d74acbe402b1d6580052ef01ef
nishu0123 <nishantnishu967@gmail.com>
2023-01-30 12:31:41 +0530

    first commit for this repositriy my protfolio

---
+++ index.js
@@ -0,0 +1,9 @@
+const home = document.getElementsByClassName("home");
+home.addEventlistner("hover", ()=>{
+    // how to add CSS for this part .
+    home.style.color = "black";
+});
+
+home.addEventlistner("click" , ()=>{
+    alert("kya bhai tm v na , home pe ho tab v home pe hi click kr rahe ho  kuch naya nahi hoga !");
+});
